214b59e86097d91cdc0931460e7ad87bb12c9be5
Katie Truong <khntruong@ucdavis.edu>
2019-05-08 10:47:39 -0700

    Updates

---
+++ precache-manifest.f8819dbca5afa4ecd943d8107b7bdb99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78a9a56012851a03ea3b12b3e6a62b7",
+    "url": "/coverme/index.html"
+  },
+  {
+    "revision": "6fc9b3c9b1bcb427bb0a",
+    "url": "/coverme/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "64044f925cb6f40e12b0",
+    "url": "/coverme/static/js/2.53622f50.chunk.js"
+  },
+  {
+    "revision": "6fc9b3c9b1bcb427bb0a",
+    "url": "/coverme/static/js/main.43ac2ccb.chunk.js"
+  },
+  {
+    "revision": "f7dba7427962d1939596",
+    "url": "/coverme/static/js/runtime~main.6ded5885.js"
+  }
+]);
